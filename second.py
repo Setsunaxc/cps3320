3829d1ee9fc4a0630d266cbb407e150a51488263
Setsunaxc <1519705108@qq.com>
2022-02-25 20:45:05 +0800

    Create second.py

---
+++ second.py
@@ -0,0 +1,11 @@
+from numpy import place
+
+
+name = input()
+year = input()
+address = input()
+favorite = input()
+print("hello, my name is"+name)
+print("I am "+year +" years")
+print("I live in "+address)
+print("I like to play "+favorite)
